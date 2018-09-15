1b0733351468ca7e5f5aa7aebc769f5b1022f851
Kyle Fang <zhigang1992@gmail.com>
2018-09-15 11:21:47 +0800

    Add native deps

--- android/app/src/main/java/com/genji/MainApplication.java
+++ android/app/src/main/java/com/genji/MainApplication.java
@@ -3,6 +3,15 @@ package com.genji;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.apsl.versionnumber.RNVersionNumberPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import cl.json.RNSharePackage;
+import com.jadsonlourenco.RNShakeEvent.RNShakeEventPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +31,16 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNVersionNumberPackage(),
+            new VectorIconsPackage(),
+            new RNSharePackage(),
+            new RNShakeEventPackage(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new RNFirebasePackage(),
+            new FastImageViewPackage(),
+            new BlurViewPackage()
       );
     }
 
