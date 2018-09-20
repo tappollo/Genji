c8949e04379f0b9537f6862fac9bf033bc14d2f4
Kyle Fang <zhigang1992@gmail.com>
2018-09-20 22:35:43 +0800

    Add site

--- site/src/App.js
+++ site/src/App.js
@@ -1,19 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  background-color: #f8f8f8;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AnimationArea = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 391px;
+  height: 662px;
+  opacity: 1;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  opacity: 1;
+  margin-left: -20px;
+`;
+
+const Title = styled.span`
+  display: flex;
+  font-size: 20px;
+  color: gray;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Subtitle = styled.span`
+  margin-top: 2px;
+  font-size: 17px;
+  color: lightgray;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const AppStore = styled.img.attrs({
+  src: require('./appstore.svg')
+})`
+  margin-top: 10px;
+  margin-left: -17px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <AnimationArea>
+          <Image src={require("./phone.png")} />
+          <Content>
+            <Title>Github</Title>
+            <Subtitle>Trending repo app</Subtitle>
+            <AppStore />
+          </Content>
+        </AnimationArea>
+      </Container>
     );
   }
 }
